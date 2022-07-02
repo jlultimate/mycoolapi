02e4c257778748c881c2f57669067d1143143eed
Jackie <ejaiscool@gmail.com>
2022-07-02 13:43:22 -0700

    (not working) hydration error

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,26 +1,42 @@
 import { google } from "googleapis";
+import { normalizeRepeatedSlashes } from "next/dist/shared/lib/utils";
 
 export async function getServerSideProps({query}){
     var auth = await google.auth.getClient({ scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"]});
     var sheets = google.sheets({version: "v4", auth});
 
     var {id} = query;
-    var range = `Sheet1!A${id}:G${id}`;
+    var range = `Sheet1!A${id}:D${id}`;
+    console.log(range);
     var response = await sheets.spreadsheets.values.get({
         spreadsheetId: process.env.SHEET_ID,
         range,
     });
     var [title, content] = response.data.values[0];
+    var all = response.data.values[0];
+    console.log(response.data.values[0]);
     return {
         props: {
             title,
-            content
-        }
+            content,
+            all
+        },
     }
 };
-export default function Post({title, content}){
+function doprint(all){
+    return (
+        <div>
+            {all.map(w => <p>{w}</p>)}
+        </div>
+    );
+    
+}
+export default function Post({title, content, all}){
     return <article>
-        <h1>{title}</h1>
-        <div dangerouslySetInnerHTML={{__html: content}}></div>
+        {/* <h1>{title}</h1>
+        <div dangerouslySetInnerHTML={{__html: content}}></div> */}
+        <p>
+            {doprint(all)}
+        </p>
     </article>;
 };
